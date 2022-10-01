d8a60fdd51621c1b5d1e6a64615a49924fa7e216
Matthew Spence <91235721+MrButak@users.noreply.github.com>
2022-10-01 14:15:14 -0500

    continued testing of env vars

--- services/dbManager.js
+++ services/dbManager.js
@@ -3,28 +3,25 @@ const { Pool } = pgPackage;
 import dotenv from "dotenv";
 dotenv.config();
 const config = useRuntimeConfig();
-// let pool = new Pool({});
 
-// if(config.DEV_ENVIRONMENT == 'development') {
-    let pool = new Pool({
+let pool = new Pool({});
+
+if(process.env.DEV_ENVIRONMENT == 'development') {
+    pool = new Pool({
         connectionString: process.env.DATABASE_URL,
-        // ssl: false
+        ssl: false
+    });
+};
+if(process.env.DEV_ENVIRONMENT == 'production') {
+
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
         ssl: {
                 required: true,
                 rejectUnauthorized: false
             }
     });
-// }
-// else {
-//     pool = new Pool({
-//         connectionString: config.DB_URL,
-//         ssl: {
-//             required: true,
-//             rejectUnauthorized: false
-//         }
-//     });
-// }
-
+};
 
 async function dbCall() {
     
