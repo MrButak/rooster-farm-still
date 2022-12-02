6e2506a17401ea3fa4c72e0a8371ace5e4273011
MrButak <mspence5555@gmail.com>
2022-12-02 16:39:19 -0600

    Changed tailwind from purge to content as specified in the documentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     mode: "jit",
-    purge: [
-      "./components/**/*.{vue,js}",
-      "./layouts/**/*.vue",
-      "./pages/**/*.vue",
-      "./plugins/**/*.{js,ts}",
-      "./nuxt.config.{js,ts}",
+    content: [
+        "./components/**/*.{vue,js}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
     ],
     // darkMode: false, // or 'media' or 'class'
     theme: {
