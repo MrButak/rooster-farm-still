0661c85d77e13f7caa33458a6307b209b6a56ce3
MrButak <mspence5555@gmail.com>
2022-12-02 16:36:04 -0600

    Added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,16 @@ export default defineNuxtConfig({
             ],
             
         }
+    },
+    css: ["~/assets/css/tailwind.css"],
+    build: {
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                },
+            },
+        },
     }
-
 });
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: "jit",
+    purge: [
+      "./components/**/*.{vue,js}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./plugins/**/*.{js,ts}",
+      "./nuxt.config.{js,ts}",
+    ],
+    // darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+}
