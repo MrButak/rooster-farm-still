c3c5ceea81f7868c53f6522c9c696ac556ccb582
MrButak <mspence5555@gmail.com>
2022-12-02 18:34:23 -0600

    Debugging color themes

--- App.vue
+++ App.vue
@@ -2,7 +2,7 @@
 
 <NuxtPage />
 <Footer />
-<!-- <va-button-toggle v-model="theme" :options="themeOptions" class="ml-2" /> -->
+<va-button-toggle v-model="theme" :options="themeOptions" class="ml-2" />
 
 </template>
 
@@ -29,50 +29,42 @@ const shoppingCartStore = useShoppingCartStore();
 // Vuestic color presets, light/dark mode, theme colors
 const { presets, applyPreset, colors, useTheme } = useColors();
 let theme = ref(null);
-applyPreset('light')
+// applyPreset('light')
 
-console.log(presets.value)
-// useHead({
-//     bodyAttrs: {
-//         style: {
-//             backgroundColor: 'red'
-//         }
-//     }
-// })
 
 // When the window Object is available
 if(process.client) {
     // applyPreset('dark')
     // Default color theme is dark
-    // uiStore.colorTheme = theme.value = window.localStorage.getItem('vuestic-docs-theme')?.toLowerCase() || 'dark';
+    uiStore.colorTheme = theme.value = window.localStorage.getItem('vuestic-docs-theme')?.toLowerCase() || 'dark';
     
-    // // If color theme is not yet in local storage, set it
-    // if(localStorageAvailable && !isItemInLs('vuestic-docs-theme')) {
-    //     setItemInLs('vuestic-docs-theme', 'dark');
-    //     uiStore.colorTheme = 'dark';
-    // };
+    // If color theme is not yet in local storage, set it
+    if(localStorageAvailable && !isItemInLs('vuestic-docs-theme')) {
+        setItemInLs('vuestic-docs-theme', 'dark');
+        uiStore.colorTheme = 'dark';
+    };
 };
 // When color theme is changed dark/light, update local storage
-// watchEffect(() => {
-//     if(localStorageAvailable) {
-//         setItemInLs('vuestic-docs-theme', theme.value);
-//         uiStore.colorTheme = theme.value;
-//     };
-//     applyPreset(theme.value)
-// });
+watchEffect(() => {
+    if(localStorageAvailable) {
+        setItemInLs('vuestic-docs-theme', theme.value);
+        uiStore.colorTheme = theme.value;
+    };
+    applyPreset(theme.value)
+});
 
 // const primaryColorVariants = ['#2c82e0', '#ef476f', '#ffd166', '#06d6a0', '#8338ec'];
 // const primaryColor = toRef(colors, 'primary');
-// let themeOptions = Object.keys(presets.value).map((themeName) => ({
-//     value: themeName,
-//     label: themeName
-// }));
+let themeOptions = Object.keys(presets.value).map((themeName) => ({
+    value: themeName,
+    label: themeName
+}));
 let red = 'red'
 let bla = red
 </script>
 
 <style lang="scss">
-
+// TODO: use a computed property for the body background-color
 body {
     // Relavtive body + absolute footer with bottom: 0 == footer stays at the bottom
     position: relative;
