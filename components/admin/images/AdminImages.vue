bc39b61153c7116859a87482e7e6afef85195241
MrButak <mspence5555@gmail.com>
2022-12-05 23:18:31 -0600

    Connected the backend to an Amazon S3 bucket, for image storage

--- components/admin/images/AdminImages.vue
+++ components/admin/images/AdminImages.vue
@@ -1,14 +1,24 @@
 <template>
 
 <h3 class="va-h3">Images</h3>
-
+<va-button @click="testS3">Test s3</va-button>
 </template>
 
 
 
 <script setup>
 
-
+async function testS3() {
+    let response = await $fetch(`/api/test-s3`, {
+        method: 'POST',
+        // body: JSON.stringify({
+        //     shipping: orderStore.userShippingData,
+        //     products: purchasedItemsObj,
+        //     subtotal: orderStore.subTotal
+        // })
+    });
+    console.log(response);
+};
 
 </script>
 
